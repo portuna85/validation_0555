1c5988aec234efd8c22c359d0b92e9afdd7c4d8e
portuna85 <portuna85@gmail.com>
2022-03-21 22:16:03 +0900

    Validation UPDATE

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -175,7 +175,7 @@ public class ValidationItemControllerV2 {
     }
 
     // @PostMapping("/add")
-    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         //검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
